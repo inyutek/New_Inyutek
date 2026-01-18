d26f9bec0cd11cf3dfd6718a7fa147d2bbe08955
Acetech0 <acem140804@gmail.com>
2026-01-18 18:33:01 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { TransitionProvider } from "./transition-provider";
 import SmoothScroll from "@/components/smooth-scroll";
@@ -20,6 +21,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-NDQ194HJKV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-NDQ194HJKV');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.className} bg-white text-[#010B13] font-sans antialiased px-4 sm:px-6 lg:px-8`}>
         <SmoothScroll>
           <Navbar />
